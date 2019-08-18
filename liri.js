b3f7252ced0c2a5472f89d32bbf2ac7dfbf2f9a3
rafaella nicoletti <rafaella_np@hotmail.com>
2019-08-18 15:51:00 -0400

    running spotify search

--- liri.js
+++ liri.js
@@ -22,7 +22,7 @@ var getSpotify = function(songName){
     if (songName === undefined){
         songName = "Whats my age again";
     }
-}
+
 
 spotify.search(
     {
@@ -41,7 +41,46 @@ function(err, data){
     for (var i  = 0; i< songs.length; i++){
         console.log(i);
         console.log("artist: " + songs[i].artists.map(getArtistNames));
-    }
+        console.log("song name: " + songs[i].name);
+        console.log("preview songs: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-------------------------------");
+        }
 
-}    
-)
+    }    
+    );
+};
+
+var getMyBands = function(artist) { 
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(
+        function(response) {
+            var jsonData = response.data;
+
+            if (!jsonData.length){
+                console.log("No results found for " + artist);
+                return;
+            }
+
+            console.log("upcoming concerts for " + artist + ":");
+
+            for (var i = 0; i> jsonData.length; i++) {
+                var show = jsonData[i];
+
+                console.log(
+                    show.venue.city + 
+                    "," + 
+                    (show.venue.region || show.venue.country) + 
+                    " at " + 
+                    show.venue.name + 
+                    " " + 
+                    moment(show.datetime).format("MM/DD/YYYY")
+                );
+            }   
+        }
+    );
+};
+//why some parenthesis end with ; and other dont? 
+
+
